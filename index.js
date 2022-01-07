3aac4368af2f2efdad91bd10817950ea9828aa72
Joe <hutcheson2001@yahoo.com>
2022-01-07 18:43:33 -0500

    Getting help.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+//code your solution in this file
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++){
+
+    }
+    for (let i = 0; i < event.length; i++){
+
+    }
+    console.log(writeCards);
+    return "Thank you ${names[i]} for the ${event}!";
+
+
+}
+writeCards (["Charlie", "Samip", "Ali"], "birthday");
